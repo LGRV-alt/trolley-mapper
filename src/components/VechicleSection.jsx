588b2ead5cac3fa83632f022ca20123f13c4fda1
LGRV-alt <114938920+LGRV-alt@users.noreply.github.com>
2024-01-06 00:28:17 +0000

    more changes

--- src/components/VechicleSection.jsx
+++ src/components/VechicleSection.jsx
@@ -1,52 +1,38 @@
 import { useState } from "react";
 
 function VechicleSection() {
-  let lorryButtons = ["4", "8", "12", "16", "20", "24"];
+  let lorryButtons = [4, 8, 12, 16, 20, 24];
   let trailerButtons = [3, 6, 7];
 
+  const [userChoice, setUserChoice] = useState("");
   const [lorry, setLorry] = useState(lorryButtons);
-  const [gridItems, setGridItems] = useState();
-
+  const [gridItems, setGridItems] = useState([]);
   const [grid, setGrid] = useState({
     col: 3,
     row: 3,
-    items: [],
-    totalTrollies: 24,
   });
 
   function handleTrailer() {
-    let totalTrollies = 7;
-    let gridItems = [];
-    for (let i = 0; i < totalTrollies; i++) {
-      gridItems.push("");
-    }
+    setGridItems([]);
     setLorry(trailerButtons);
     setGrid({
-      items: gridItems,
       col: 3,
       row: 3,
     });
-    console.log(grid);
   }
 
   function handleLorry() {
-    let totalTrollies = 24;
-    let gridItems = [];
-    for (let i = 0; i < totalTrollies; i++) {
-      gridItems.push("");
-    }
     setLorry(lorryButtons);
-    setGrid({ col: 4, row: 6, items: gridItems });
-    console.log(grid);
+    setGrid({ col: 4, row: 6 });
+    setGridItems([]);
   }
 
   function generateGrid(number) {
-    setGridItems(number);
     let arr = [];
     for (let i = 0; i < number; i++) {
-      arr.push(i);
+      arr.push("");
     }
-    console.log(arr);
+    setGridItems(arr);
   }
 
   return (
@@ -61,9 +47,13 @@ function VechicleSection() {
       <div
         className={` outline outline-white h-3/4 w-3/4 mt-8 grid grid-cols-${grid.col} grid-rows-${grid.row} `}
       >
-        {grid.items.map(() => (
-          <div key="2" className="outline outline-white">
-            {gridItems}
+        {gridItems.map(() => (
+          <div
+            onClick={() => console.log(userChoice)}
+            key="2"
+            className="outline outline-white"
+          >
+            {userChoice}
           </div>
         ))}
       </div>
