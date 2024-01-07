02d58186e6254bb9d0b5dbde5d3136ffa32b4992
LGRV-alt <114938920+LGRV-alt@users.noreply.github.com>
2024-01-07 00:21:17 +0000

    small changes
    small changes to the form

--- src/components/NewCustomerButton.jsx
+++ src/components/NewCustomerButton.jsx
@@ -2,9 +2,16 @@ import { useState } from "react";
 
 function NewCustomerButton() {
   const [customer, setCustomer] = useState({});
+  const [form, setForm] = useState({});
 
   function handleCreate() {
     setCustomer({});
+    setForm({ name: "" });
+  }
+
+  function handleFormInputs(event, inputName) {
+    setCustomer({ ...customer, [inputName]: event.target.value });
+    setForm({ ...form, name: event.target.value });
   }
   return (
     <div className="border-2 border-white w-full h-12 mb-8">
@@ -12,31 +19,38 @@ function NewCustomerButton() {
         <label>
           Name:
           <input
+            value={form.name}
             type="text"
             name="name"
             placeholder="Landscapers"
-            onChange={(e) => setCustomer({ ...customer, name: e.target.value })}
+            onChange={(event) => handleFormInputs(event)}
           />
         </label>
         <label>
           Trollies:
           <input
+            value={form.trollies}
             type="number"
             name="trollies"
             placeholder="4T"
-            onChange={(e) =>
-              setCustomer({ ...customer, trollies: e.target.value })
-            }
+            onChange={(event) => handleFormInputs(event)}
           />
         </label>
         <label>
           Extras:
-          <input type="text" name="extras" placeholder="10 Comp" />
+          <input
+            value={form.extras}
+            type="text"
+            name="extras"
+            placeholder="10 Comp"
+            onChange={(event) => handleFormInputs(event, extras)}
+          />
         </label>
       </form>
       <button onClick={() => handleCreate()}>create</button>
       <p>{customer.name}</p>
       <p>{customer.trollies}</p>
+      <p>Extras: {customer.trollies}</p>
     </div>
   );
 }
