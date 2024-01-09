9e955cacbe886fcde56309cc0d77e963b1356871
LGRV-alt <114938920+LGRV-alt@users.noreply.github.com>
2024-01-09 23:36:32 +0000

    changes to the customer inputs

--- src/components/NewCustomerForm.jsx
+++ src/components/NewCustomerForm.jsx
@@ -19,48 +19,49 @@ function NewCustomerButton({ setCustomerList, customerList }) {
     setForm({ ...form, [key]: event.target.value });
   }
   return (
-    <div className="border-2 border-white ">
+    <div className="border-2 border-white min-w-60 ">
       <form className="grid grid-cols-1 grid-rows-3 mt-4 ml-4">
-        <label className="">
-          Name:
+        <div className="w-full flex flex-col">
+          <label className="flex ">Customer Name</label>
           <input
-            className="mb-4 "
+            className="mb-2 w-2/3"
             value={form.name}
             type="text"
             name="name"
             placeholder="Landscapers"
             onChange={(event) => handleFormInputs(event, "name")}
           />
-        </label>
-        <label className="flex ">
-          Trollies:
+        </div>
+        <div className="w-full flex flex-col ">
+          <label className="flex">Extras</label>
           <input
-            className=" mb-4 w-12 "
-            value={form.trollies}
-            type="number"
-            name="trollies"
-            placeholder="4T"
-            onChange={(event) => handleFormInputs(event, "trollies")}
-          />
-        </label>
-        <label className="flex justify-between mb-4">
-          Extras:
-          <input
-            className="mr-6"
+            className="mb-2 w-2/3"
             value={form.extras}
             type="text"
             name="extras"
             placeholder="10 Comp"
             onChange={(event) => handleFormInputs(event, "extras")}
           />
-        </label>
+        </div>
+        <div className="w-full flex  flex-col ">
+          <label className="flex ">Trollies</label>
+          <input
+            className="mb-2 w-1/3"
+            value={form.trollies}
+            type="number"
+            name="trollies"
+            placeholder="4T"
+            onChange={(event) => handleFormInputs(event, "trollies")}
+          />
+        </div>
+        <button
+          type="button"
+          className=" w-2/3 mb-3 h-8 bg-black text-white hover:bg-white hover:text-black"
+          onClick={() => handleCreate()}
+        >
+          create
+        </button>
       </form>
-      <button
-        className=" w-full bg-black text-white "
-        onClick={() => handleCreate()}
-      >
-        create
-      </button>
     </div>
   );
 }
