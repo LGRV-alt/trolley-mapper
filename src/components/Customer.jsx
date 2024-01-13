77729030d50601d1fa07ae2ef50546fe89ffba90
LGRV-alt <114938920+LGRV-alt@users.noreply.github.com>
2024-01-13 19:58:42 +0000

    changes to customer layout
    changed the layout of the customer values

--- src/components/Customer.jsx
+++ src/components/Customer.jsx
@@ -14,24 +14,29 @@ function Customer({ customerList, setCustomerList, onClick }) {
         <div
           key={index}
           onClick={() => onClick([item.name])}
-          className=" lg:w-1/2 w-full h-6 lg:h-11 lg:grid lg:grid-rows-1 lg:grid-cols-[20%_40%_30%_20%] flex gap-2"
+          className=" lg:w-1/2 w-full h-6 lg:h-11 lg:grid lg:grid-rows-1 lg:grid-cols-2 flex gap-2"
         >
-          <button
-            className="text-red-600 hover:text-red-500"
-            onClick={() => handleDel(index)}
-          >
-            X
-          </button>
-          <h3 className="flex lg:text-lg ml-2 items-center sm:text-base  hover:text-blue-500 hover:cursor-pointer">
-            {item.name}
-          </h3>
-          {item.trollies ? (
-            <p className="text-red-500 flex justify-center items-center ">
-              {" "}
-              {item.trollies}T
-            </p>
-          ) : null}
-          <p className="text-red-500 flex  items-center">{item.extras}</p>
+          <div className="flex flex-row gap-2 ml-5">
+            <button
+              className="w-2/12 text-red-600 hover:bg-red-600 hover:text-white hover:rounded-full "
+              onClick={() => handleDel(index)}
+            >
+              X
+            </button>
+            <h3 className="flex lg:text-lg  items-center sm:text-base  hover:text-blue-500 hover:cursor-pointer">
+              {item.name}
+            </h3>
+          </div>
+
+          <div className="flex flex-row gap-2">
+            {item.trollies ? (
+              <p className="text-red-500 flex justify-center items-center ">
+                {" "}
+                {item.trollies}T
+              </p>
+            ) : null}
+            <p className="text-yellow-500 flex items-center">{item.extras}</p>
+          </div>
         </div>
       ))}
     </div>
