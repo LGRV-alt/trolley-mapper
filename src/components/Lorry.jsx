7c883988f135e8e3bf94aeaf19a0ccbcb4730498
LGRV-alt <114938920+LGRV-alt@users.noreply.github.com>
2024-01-26 12:16:33 +0000

    style - resize the lorry grid when full screen

--- src/components/Lorry.jsx
+++ src/components/Lorry.jsx
@@ -3,7 +3,7 @@ import GridItem from "./GirdItem";
 function LorryMap({ customerName, grid }) {
   return (
     <div
-      className={` border-2 lg:h-4/5 lg:w-4/5 sm:h-full w-full h-2/4 mt-8 grid grid-cols-4 grid-rows-6 `}
+      className={` border-2 lg:h-[75%] lg:w-[75%] sm:h-full w-full h-2/4 mt-4 grid grid-cols-4 grid-rows-6 `}
     >
       {grid.map((item, index) => (
         <div key={index} className="border-2 ">
