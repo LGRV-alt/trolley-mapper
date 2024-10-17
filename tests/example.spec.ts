606ceb0bd69bbcd3af3a4d9cb257beb0c784d21c
LGRV-alt <114938920+LGRV-alt@users.noreply.github.com>
2024-10-17 22:00:38 +0100

    working on tests

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,5 +1,9 @@
 import { test, expect } from "@playwright/test";
 
+test.beforeEach(async ({ page }) => {
+  await page.goto("http://localhost:5173/");
+});
+
 const customerValues = [
   { name: "lewis", trollies: "3", other: "2 cages" },
   { name: "john", trollies: "2", other: "5 pallets" },
@@ -7,27 +11,28 @@ const customerValues = [
   { name: "phil", trollies: "2", other: "Lots of loose" },
 ];
 
-test("Trolley Mapper Title", async ({ page }) => {
-  await page.goto("http://localhost:5173/");
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Trolley Mapper/);
-});
+test.describe("Testing array", () => {
+  test("Testing multiple entries coming from an array", async ({ page }) => {
+    await page.getByRole("button", { name: "Add Customer" }).click();
+    for (const customer of customerValues) {
+      await page.getByPlaceholder("Landscapers").fill(customer.name);
+      await page.getByPlaceholder("4T").fill(customer.trollies);
+      await page.getByPlaceholder("Comp").fill(customer.other);
+      await page.getByRole("button", { name: "create" }).click();
+    }
+    await page.getByRole("button", { name: "Add Customer" }).click();
+    await page
+      .getByRole("button", { name: "X 3 - Jane 5T" })
+      .getByRole("button")
+      .click();
+    await expect(page.locator("#content")).toContainText("Total Trolleys- 7");
+  });
 
-test("Testing multiple entries coming from an array", async ({ page }) => {
-  await page.goto("http://localhost:5173/");
-  await page.getByRole("button", { name: "Add Customer" }).click();
-  for (const customer of customerValues) {
-    await page.getByPlaceholder("Landscapers").fill(customer.name);
-    await page.getByPlaceholder("4T").fill(customer.trollies);
-    await page.getByPlaceholder("Comp").fill(customer.other);
-    await page.getByRole("button", { name: "create" }).click();
-  }
-  await page.getByRole("button", { name: "Add Customer" }).click();
-  await page
-    .getByRole("button", { name: "X 3 - Jane 5T" })
-    .getByRole("button")
-    .click();
-  await expect(page.locator("#content")).toContainText("Total Trolleys- 7");
+  test("Trolley Mapper Title", async ({ page }) => {
+    await page.goto("http://localhost:5173/");
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/Trolley Mapper/);
+  });
 });
 
 test("Input trolley number and check its shown on the total", async ({
