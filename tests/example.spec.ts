8ce5cadd152a17add2711627c15cf1f3064dcdfb
LGRV-alt <114938920+LGRV-alt@users.noreply.github.com>
2024-10-17 20:52:14 +0100

    array test
    adding a test that takes an array full of values and passes it onto the page

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,9 +1,29 @@
 import { test, expect } from "@playwright/test";
 
+const customerValues = [
+  { name: "lewis", trollies: "3", other: "2 cages" },
+  { name: "john", trollies: "2", other: "5 pallets" },
+  { name: "jane", trollies: "5", other: "" },
+  { name: "phil", trollies: "2", other: "Lots of loose" },
+];
+
 test("Trolley Mapper Title", async ({ page }) => {
   await page.goto("http://localhost:5173/");
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Trolley Mapper/);
+});
+
+test("Testing multiple entries coming from an array", async ({ page }) => {
+  await page.goto("http://localhost:5173/");
+  await page.getByRole("button", { name: "Add Customer" }).click();
+  for (const customer of customerValues) {
+    await page.getByPlaceholder("Landscapers").fill(customer.name);
+    await page.getByPlaceholder("4T").fill(customer.trollies);
+    await page.getByPlaceholder("Comp").fill(customer.other);
+    await page.getByRole("button", { name: "create" }).click();
+  }
+  await page.getByRole("button", { name: "Add Customer" }).click();
+  await expect(page.locator("#content")).toContainText("Total Trolleys- 12");
 });
 
 test("Input trolley number and check its shown on the total", async ({
